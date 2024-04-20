92fe9f5eb907bceabaa106c6e696a27f736fb1af
Aakash Pal <aakashpal1183@gmail.com>
2024-04-21 01:34:28 +0530

    Style HTML User intrface

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
+from jobs import JOBS
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', jobs=JOBS)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ jobs.py
@@ -0,0 +1,20 @@
+JOBS=[
+    {
+        'id':1, 
+        'title':'Data Scientist',
+        'location':'New York, USA',
+        'salary':'Rs. 150000'
+    }, 
+    {
+        'id':2, 
+        'title':'Data Engineer',
+        'location':'San Francisco, USA',
+        'salary':'Rs. 200000'
+    }, 
+    {
+        'id':3, 
+        'title':'Data Analyst',
+        'location':'Los Angeles, USA',
+        'salary':'Rs. 100000'
+    }
+]
